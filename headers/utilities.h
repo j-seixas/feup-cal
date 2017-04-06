26ca79c8d5dfc7f1808c544a582276f633d35678
rendoir <up201503822@fe.up.pt>
2017-04-06 20:45:40 +0100

    Added algorithms to determine a accurate location of the nodes in the graph viewer (instead of random).

--- headers/utilities.h
+++ headers/utilities.h
@@ -19,13 +19,47 @@ string getStreetName();
 uint16 getInput();
 void printSquareArray(int ** arr, unsigned int size);
 
+static bool init = false;
+static double minLatitute;
+static double maxLatitude;
+static double minLongitude;
+static double maxLongitude;
+const int WIDTH = 1000;
+const int HEIGHT = 800;
+
+template<class T>
+pair<int, int> calculatePosition(Vertex<T>* v) {
+	int x = (v->getLatitude() - minLatitute) * WIDTH / (maxLatitude - minLatitute);
+	int y = (v->getLongitude() - minLongitude) * HEIGHT / (maxLongitude - minLongitude);
+	return pair<int,int>(x, y);
+}
+
+template<class T>
+void updateBounds(const Vertex<T> *v) {
+	if (init) {
+		if (v->getLatitude() < minLatitute)
+			minLatitute = v->getLatitude();
+		else if (v->getLatitude() > maxLatitude)
+			maxLatitude = v->getLatitude();
+		if (v->getLongitude() < minLongitude)
+			minLongitude = v->getLongitude();
+		else if (v->getLongitude() > maxLongitude)
+			maxLongitude = v->getLongitude();
+	} else {
+		minLatitute = v->getLatitude();
+		maxLatitude = v->getLatitude();
+		minLongitude = v->getLongitude();
+		maxLongitude = v->getLongitude();
+		init = true;
+	}
+}
 template<class T>
 void loadNodes(Graph<T> &graph) {
 	//Format: nodeID;latitudeDegrees;longitudeDegrees;longitudeRadians;latitudeRadians
 	string line;
 	ifstream file;
 	file.open("rsc/Nodes.txt");
-	if (!file.is_open()){
+	if (!file.is_open()) {
 		cout << "Failed to open Node txt file!\n";
 		exit(1);
 	}
@@ -38,7 +72,9 @@ void loadNodes(Graph<T> &graph) {
 		iss >> nodeID >> delimiter >> latitudeDegrees >> delimiter
 				>> longitudeDegrees >> delimiter >> longitudeRadians
 				>> delimiter >> latitudeRadians;
-		graph.addVertex(new Vertex<T>(nodeID, latitudeRadians, longitudeRadians));
+		Vertex<T> *v = new Vertex<T>(nodeID, latitudeRadians, longitudeRadians);
+		updateBounds(v);
+		graph.addVertex(v);
 	}
 	file.close();
 }
@@ -60,8 +96,9 @@ void loadEdges(Graph<T> &graph) {
 		iss >> edgeID >> delimiter >> srcID >> delimiter >> dstID;
 		Vertex<T>* src = graph.getVertex(srcID);
 		Vertex<T>* dst = graph.getVertex(dstID);
-		if(src != nullptr && dst != nullptr)
-			graph.addEdge(new Edge<T>(dst, edgeID, calculateDistance(src, dst)), src);
+		if (src != nullptr && dst != nullptr)
+			graph.addEdge(new Edge<T>(dst, edgeID, calculateDistance(src, dst)),
+					src);
 	}
 	file.close();
 }
@@ -87,16 +124,18 @@ void loadStreets(Graph<T> &graph) {
 		getline(iss, isTwoWaysStr);
 		isTwoWays = (isTwoWaysStr == "True");
 		for (uint64 i = 0; i < graph.getVertexSet().size(); i++) {
-			Vertex<T> *vertex =  graph.getVertexSet()[i];
-			for (uint64 j = 0;	j < vertex->getAdjacent().size(); j++) {
+			Vertex<T> *vertex = graph.getVertexSet()[i];
+			for (uint64 j = 0; j < vertex->getAdjacent().size(); j++) {
 				Edge<T> *edge = &vertex->getAdjacent()[j];
 				if (edge->getID() == edgeID) {
 					edge->setName(streetName);
 					edge->setNameMask(nextStreetName());
 					edge->setTwoWays(isTwoWays);
-					if(isTwoWays){
+					if (isTwoWays) {
 						//If it's two ways, create another edge which is the opposite of the original and so is its ID.
-						Edge<T>* oppositeEdge = new Edge<T>(vertex, -1 * edge->getID(), calculateDistance(vertex, edge->getDest()));
+						Edge<T>* oppositeEdge = new Edge<T>(vertex,
+								-1 * edge->getID(),
+								calculateDistance(vertex, edge->getDest()));
 						oppositeEdge->setName(streetName);
 						oppositeEdge->setNameMask(nextStreetName());
 						graph.addEdge(oppositeEdge, edge->getDest());
@@ -124,6 +163,5 @@ double calculateDistance(Vertex<T> *v1, Vertex<T> *v2) {
 	double c = earthRadius * b;
 	return c;
 }
-
 
 #endif // UTILITIES_H
